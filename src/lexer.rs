32680b97a95b021df76fe24984f480b4dd5f9af2
AnsonH <ansonheung2000@gmail.com>
2024-11-24 20:48:23 +0800

    feat(lexer): slash and single line comments

--- src/lexer.rs
+++ src/lexer.rs
@@ -32,8 +32,15 @@ impl<'de> Lexer<'de> {
         self.position += ch_len;
         Some((ch, ch_len))
     }
+
+    /// Peeks the next char to see if it is end of file.
+    #[inline]
+    fn is_peek_char_eof(&mut self) -> bool {
+        self.rest_chars.peek().is_none()
+    }
 }
 
+/// Scenarios that requires scanning multiple characters to determine the token.
 enum Started {
     /// Token kind is `matched` if next char is `to_match`, else is `unmatched`.
     MatchNextChar {
@@ -41,6 +48,7 @@ enum Started {
         matched: TokenKind,
         unmatched: TokenKind,
     },
+    Slash,
 }
 
 impl<'de> Iterator for Lexer<'de> {
@@ -52,73 +60,91 @@ impl<'de> Iterator for Lexer<'de> {
             // Rust will stop the `for token in lexer {}` loop if `.next()` returns `None`.
             return None;
         }
-        if self.rest_chars.peek().is_none() {
-            self.position += 1;
-            return Some(Ok(Token::new(TokenKind::Eof, "")));
-        }
 
-        let (ch, ch_len) = self.read_char()?;
-        let ch_str = self.input.get((self.position - ch_len)..self.position)?;
-
-        let just = |kind: TokenKind| Some(Ok(Token::new(kind, ch_str)));
-
-        let started = match ch {
-            '(' => return just(TokenKind::LeftParen),
-            ')' => return just(TokenKind::RightParen),
-            '{' => return just(TokenKind::LeftBrace),
-            '}' => return just(TokenKind::RightBrace),
-            ',' => return just(TokenKind::Comma),
-            ';' => return just(TokenKind::Semicolon),
-            '.' => return just(TokenKind::Dot),
-            '+' => return just(TokenKind::Plus),
-            '-' => return just(TokenKind::Minus),
-            '*' => return just(TokenKind::Star),
-            '/' => return just(TokenKind::Slash),
-            '=' => Started::MatchNextChar {
-                to_match: '=',
-                matched: TokenKind::EqualEqual,
-                unmatched: TokenKind::Equal,
-            },
-            '!' => Started::MatchNextChar {
-                to_match: '=',
-                matched: TokenKind::BangEqual,
-                unmatched: TokenKind::Bang,
-            },
-            '<' => Started::MatchNextChar {
-                to_match: '=',
-                matched: TokenKind::LessEqual,
-                unmatched: TokenKind::Less,
-            },
-            '>' => Started::MatchNextChar {
-                to_match: '=',
-                matched: TokenKind::GreaterEqual,
-                unmatched: TokenKind::Greater,
-            },
-            c => {
-                return Some(Err(SyntaxError::SingleTokenError {
-                    token: c,
-                    err_span: (self.position - ch_len, ch_len).into(), // (offset, length)
-                }));
+        loop {
+            if self.is_peek_char_eof() {
+                self.position += 1;
+                return Some(Ok(Token::new(TokenKind::Eof, "")));
             }
-        };
 
-        match started {
-            Started::MatchNextChar {
-                to_match,
-                matched,
-                unmatched,
-            } => {
-                if matches!(self.rest_chars.peek(), Some(&c) if c == to_match) {
-                    let (_, next_ch_len) = self.read_char()?;
-                    let lexeme = self
-                        .input
-                        .get((self.position - next_ch_len - ch_len)..self.position)?;
+            let (ch, ch_len) = self.read_char()?;
+            let ch_str = self.input.get((self.position - ch_len)..self.position)?;
 
-                    Some(Ok(Token::new(matched, lexeme)))
-                } else {
-                    just(unmatched)
+            let just = |kind: TokenKind| Some(Ok(Token::new(kind, ch_str)));
+
+            let started = match ch {
+                '(' => return just(TokenKind::LeftParen),
+                ')' => return just(TokenKind::RightParen),
+                '{' => return just(TokenKind::LeftBrace),
+                '}' => return just(TokenKind::RightBrace),
+                ',' => return just(TokenKind::Comma),
+                ';' => return just(TokenKind::Semicolon),
+                '.' => return just(TokenKind::Dot),
+                '+' => return just(TokenKind::Plus),
+                '-' => return just(TokenKind::Minus),
+                '*' => return just(TokenKind::Star),
+                '/' => Started::Slash,
+                '=' => Started::MatchNextChar {
+                    to_match: '=',
+                    matched: TokenKind::EqualEqual,
+                    unmatched: TokenKind::Equal,
+                },
+                '!' => Started::MatchNextChar {
+                    to_match: '=',
+                    matched: TokenKind::BangEqual,
+                    unmatched: TokenKind::Bang,
+                },
+                '<' => Started::MatchNextChar {
+                    to_match: '=',
+                    matched: TokenKind::LessEqual,
+                    unmatched: TokenKind::Less,
+                },
+                '>' => Started::MatchNextChar {
+                    to_match: '=',
+                    matched: TokenKind::GreaterEqual,
+                    unmatched: TokenKind::Greater,
+                },
+                c => {
+                    return Some(Err(SyntaxError::SingleTokenError {
+                        token: c,
+                        err_span: (self.position - ch_len, ch_len).into(), // (offset, length)
+                    }));
                 }
-            }
+            };
+
+            // `break match` = Return the match statement's value back to the loop
+            break match started {
+                Started::MatchNextChar {
+                    to_match,
+                    matched,
+                    unmatched,
+                } => {
+                    if self.rest_chars.peek() == Some(&to_match) {
+                        let (_, next_ch_len) = self.read_char()?;
+                        let lexeme = self
+                            .input
+                            .get((self.position - next_ch_len - ch_len)..self.position)?;
+
+                        Some(Ok(Token::new(matched, lexeme)))
+                    } else {
+                        just(unmatched)
+                    }
+                }
+                Started::Slash => {
+                    if self.rest_chars.peek() == Some(&'/') {
+                        loop {
+                            let is_line_end_reached = self.is_peek_char_eof()
+                                || matches!(self.read_char(), Some((ch, _)) if ch == '\n');
+                            if is_line_end_reached {
+                                break;
+                            }
+                        }
+                        continue;
+                    } else {
+                        just(TokenKind::Slash)
+                    }
+                }
+            };
         }
     }
 }
@@ -206,6 +232,30 @@ mod tests {
             "EOF  null",
         ];
         assert_tokens(input, &expected);
+    }
+
+    #[test]
+    fn test_single_line_comment() {
+        let empty_comment = "//";
+        let expected = vec!["EOF  null"];
+        assert_tokens(empty_comment, &expected);
+
+        let comment_till_eof = "// Hi";
+        let expected = vec!["EOF  null"];
+        assert_tokens(comment_till_eof, &expected);
+
+        let multi_line_with_comments = "(// Hi\n.// Bye\n+";
+        let expected = vec![
+            "LEFT_PAREN ( null",
+            "DOT . null",
+            "PLUS + null",
+            "EOF  null",
+        ];
+        assert_tokens(multi_line_with_comments, &expected);
+
+        let not_comment = "/(";
+        let expected = vec!["SLASH / null", "LEFT_PAREN ( null", "EOF  null"];
+        assert_tokens(not_comment, &expected);
     }
 
     #[test]
